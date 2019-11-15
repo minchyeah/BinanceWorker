fce576a1710c17131acd294ea8cbc0f814a7d707
梁敏成 <yeah@minch.me>
2019-11-15 11:24:44 +0800

    update

--- Config/Bianace.php
+++ Config/Bianace.php
@@ -3,36 +3,20 @@
 namespace Config;
 
 /**
- * GlobalData变量共享组件配置
- * API 密钥：
- * PFnZzFG3BuEjixBkYUHKvFPBBhbl9CJ28lqpET8oX93qgfbulEpP43j77pLmYhJD
- * 密钥:
- * OJMGRALLVial4JqJRk9vcBALXJFL8oO6dEzxHCBmNdtFwwMMtbyRv2b7vUEJyaIO
+ * Binance接口配置
  * @author minch
  */
 class Binance
 {
     /**
-     * 监听的ip地址，分布式部署时使用内网ip
+     * API KEY
      * @var string
      */
-    public static $address = '127.0.0.1';
+    public static $api_key = 'PFnZzFG3BuEjixBkYUHKvFPBBhbl9CJ28lqpET8oX93qgfbulEpP43j77pLmYhJD';
 
     /**
-     * 网关监听端口
-     * @var number
-     */
-    public static $port = 12207;
-
-    /**
-     * 数据是否持久化
-     * @var boolean
-     */
-    public static $persistence = true;
-
-    /**
-     * 数据持久化文件路径
+     * 加密密钥
      * @var string
      */
-    public static $datapath = '/tmp/GlobalData_cache.php';
+    public static $api_secret = 'OJMGRALLVial4JqJRk9vcBALXJFL8oO6dEzxHCBmNdtFwwMMtbyRv2b7vUEJyaIO';
 }
